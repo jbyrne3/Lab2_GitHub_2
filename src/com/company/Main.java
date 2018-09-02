bdb8f76dca387cde33d85ba09ed50ca89e39be64
John-Paul Byrne <john-paulbyrne@John-Pauls-Air.attlocal.net>
2018-09-02 16:38:14 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Simple hello world program to use with GitHub exercise
+       // System.out.println("Hello World");
+
+
+    }
+}
+`
