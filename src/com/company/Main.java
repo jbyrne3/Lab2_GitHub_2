6e8bda146cbb4d03930fd7e90f3c4ed88cb444a8
jbyrne3 <42792524+jbyrne3@users.noreply.github.com>
2018-09-02 16:48:39 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// Simple hello world program to use with GitHub exercise
-       // System.out.println("Hello World");
+       System.out.println("Hello World");
 
 
     }
